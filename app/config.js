35b130fd95401d2fe570753134a15b8c84321644
santiagocarrilloT <santiago.carrillo@correounivalle.edu.co>
2024-09-12 22:56:02 -0500

    cambioProyecto

--- app/config.js
+++
@@ -1,8 +0,0 @@
-import { NativeWindStyleSheet } from "nativewind";
-import { registerRootComponent } from "expo";
-
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
-
-export { registerRootComponent };
